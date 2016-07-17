16209335586a71bced52279a1dd561c27e475cfc
Vincent <vincent.gon@orange.fr>
2016-07-17 13:09:10 +0200

    Add code comments

--- server/main.js
+++ server/main.js
@@ -6,16 +6,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var router = express.Router();
-router.get('/', function(req, res) { 
-    res.json({ message: 'DoIt! API' });   
+router.get('/', function (req, res) {
+  res.json({ message: 'DoIt! API' });
 });
 
+// Allow CORS
+app.use(function(req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// User route
 router.route('/user')
-  .post(function(req, res){
-      console.log("post made on user");
+  .post(function (req, res) {
+    res.json({ success: false, message: 'toto' });
   });
+
+// Define API path
 app.use('/api', router);
 
+// Listen the port
 app.listen(3000, function () {
-  console.log('The API server is running on the port 3000 !');  
+  console.log('The API server is running on the port 3000 !');
 }); 
